3c392fb3793a2a7c2daa267f901195cd1bfba55f
Fernando Gurgel <gurgelff@gmail.com>
2021-06-12 23:43:57 -0300

    FEAT: depurando o estado | Async useState

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,12 @@ import { Chance } from "chance";
 import Head from "next/head";
 
 export default function Home() {
+  let usuarios_dados = []
+  //@TODO: substituir todas as evaluações do estado
+  //       de usuarios para usuarios_dados, de modo
+  //       que o estado seja alterado apenas ao final,
+  //       pois o usestate é assícrono.
+
   const [usuarios, set_usuarios] = useState([]);
   const video_id = "27716306792649728";
   const email_regex = new RegExp(/^[a-z0-9.]+@[a-z0-9]+.[a-z]+.([a-z]+)?$/i);
@@ -16,237 +22,289 @@ export default function Home() {
   const get_likes = async (params) => await axios.get("api/get_like", params);
   const get_gift_votes = async () => await axios.get("api/get_vip");
 
-  const checar_comentarios = async () => {
-    console.log("obtendo comentários...");
-    const dados_comentarios = await get_comentarios();
-    const lista_de_comentadores = dados_comentarios.data.data.list;
-
-    for (const comentador of lista_de_comentadores) {
-      let ja_existe = false;
-      let indice = 0;
-
-      for (let usuario of usuarios) {
-        if (comentador.uid == usuario.id) {
-          ja_existe = true;
-          indice = usuarios.indexOf(usuario);
-        }
-      }
-
-      if (!ja_existe) {
-        const comentador_estruturado = {
-          nome: comentador.user.nickname,
-          conta: comentador.user.chain_aacount_name,
-          id: comentador.uid,
-          tickets: 0,
-          segue: false,
-          deu_like: false,
-          comentou: true,
-          nome_valido: false,
-          mensagem: [],
-        };
-        set_usuarios((old_usuarios) => [
-          ...old_usuarios,
-          comentador_estruturado,
-        ]);
-      } else {
-        let usuarios_tmp = [...usuarios];
-        usuarios_tmp[indice].comentou = true;
-        set_usuarios(usuarios_tmp);
-
-      }
-    }
-
+  const checar_comentarios = () => {
     return new Promise((resolve, reject) => {
-      resolve("ok");
+      (async () => {
+        try {
+          console.log("obtendo comentários...");
+          const dados_comentarios = await get_comentarios();
+          const lista_de_comentadores = dados_comentarios.data.data.list;
+
+          for (const comentador of lista_de_comentadores) {
+            let ja_existe = false;
+            let indice = 0;
+
+            for (let usuario of usuarios) {
+              if (comentador.uid == usuario.id) {
+                ja_existe = true;
+                indice = usuarios.indexOf(usuario);
+              }
+            }
+
+            if (!ja_existe) {
+              const comentador_estruturado = {
+                nome: comentador.user.nickname,
+                conta: comentador.user.chain_account_name,
+                id: comentador.uid,
+                tickets: 0,
+                segue: false,
+                deu_like: false,
+                comentou: true,
+                nome_valido: false,
+                mensagem: [],
+              };
+              set_usuarios((old_usuarios) => [
+                comentador_estruturado,
+                ...old_usuarios,
+              ]);
+            } else {
+              let usuarios_tmp = [...usuarios];
+              usuarios_tmp[indice].comentou = true;
+              set_usuarios(usuarios_tmp);
+            }
+          }
+          console.log("user comments ", usuarios);
+          resolve(usuarios);
+        } catch (error) {
+          reject(error);
+        }
+      })();
     });
   };
 
-  const checar_inscritos = async () => {
-    console.log("obtendo inscritos...");
-    const dados_comentarios = await get_inscritos();
-    const lista_de_inscritos = dados_comentarios.data.data.list;
-
-    for (const inscrito of lista_de_inscritos) {
-      let ja_existe = false;
-      let indice = 0;
-
-      for (let usuario of usuarios) {
-        if (inscrito.uid == usuario.id) {
-          ja_existe = true;
-          indice = usuarios.indexOf(usuario);
-        }
-      }
-
-      if (!ja_existe) {
-        let usuario_estruturado = {
-          nome: inscrito.nickname,
-          conta: "",
-          id: inscrito.uid,
-          tickets: 0,
-          segue: true,
-          deu_like: false,
-          comentou: false,
-          nome_valido: false,
-          mensagem: [],
-        };
-        usuario_estruturado.mensagem.push("Não comentou. ");
-        set_usuarios((old_usuarios) => [...old_usuarios, usuario_estruturado]);
-      } else {
-        let usuarios_tmp = [...usuarios];
-        usuarios_tmp[indice].segue = true;
-        set_usuarios(usuarios_tmp);
-      }
-    }
-
+  const checar_inscritos = () => {
     return new Promise((resolve, reject) => {
-      resolve("ok");
+      (async () => {
+         try {
+           console.log("obtendo inscritos...");
+           const dados_comentarios = await get_inscritos();
+           const lista_de_inscritos = dados_comentarios.data.data.list;
+
+           for (const inscrito of lista_de_inscritos) {
+             let ja_existe = false;
+             let indice = 0;
+
+             for (let usuario of usuarios) {
+               if (inscrito.uid == usuario.id) {
+                 ja_existe = true;
+                 indice = usuarios.indexOf(usuario);
+               }
+             }
+
+             if (!ja_existe) {
+               let usuario_estruturado = {
+                 nome: inscrito.nickname,
+                 conta: "",
+                 id: inscrito.uid,
+                 tickets: 0,
+                 segue: true,
+                 deu_like: false,
+                 comentou: false,
+                 nome_valido: false,
+                 mensagem: [],
+               };
+               usuario_estruturado.mensagem.push("Não comentou. ");
+               set_usuarios((old_usuarios) => [
+                 usuario_estruturado,
+                 ...old_usuarios,
+               ]);
+             } else {
+               let usuarios_tmp = [...usuarios];
+               usuarios_tmp[indice].segue = true;
+               set_usuarios(usuarios_tmp);
+             }
+           }
+           console.log("user subs ", usuarios);
+           resolve(usuarios);
+
+         } catch (error) {
+           reject(error);
+           
+         }
+         
+       })();
+      
     });
   };
 
   const checar_nomes = () => {
-    console.log("checando nomes...");
-    console.log(usuarios);
-
-    for (const usuario of usuarios) {
-      let usuarios_tmp = [...usuarios];
-      const indice = usuarios.indexOf(usuario);
-      
-      if (
-        email_regex.test(usuario.nome) ||
-        asterisco_regex.test(usuario.nome)
-      ) {
-        usuarios_tmp[indice].nome_valido = false;
-        usuarios_tmp[indice].mensagem.push("Nome inválido. ");
-        set_usuarios(usuarios_tmp);
-      } else {
-        usuarios_tmp[indice].nome_valido = true;
-        set_usuarios(usuarios_tmp);
-      }
-    }
-    
     return new Promise((resolve, reject) => {
-      resolve("ok");
+      try {
+        console.log("checando nomes...");
+
+        for (const usuario of usuarios) {
+          let usuarios_tmp = [...usuarios];
+          const indice = usuarios.indexOf(usuario);
+
+          if (
+            email_regex.test(usuario.nome) ||
+            asterisco_regex.test(usuario.nome)
+          ) {
+            usuarios_tmp[indice].nome_valido = false;
+            usuarios_tmp[indice].mensagem.push("Nome inválido. ");
+            set_usuarios(usuarios_tmp);
+          } else {
+            usuarios_tmp[indice].nome_valido = true;
+            set_usuarios(usuarios_tmp);
+          }
+        }
+        console.log("user names ", usuarios);
+
+        resolve(usuarios);
+
+      } catch (error) {
+        reject(error);
+        
+      }
+
+      
     });
   };
 
-  const checar_likes = async () => {
-    for (const usuario of usuarios) {
-      const usuarios_tmp = usuarios;
-      const indice = usuarios.indexOf(usuario);
+  const checar_likes = () => {
+    console.log("likes, usuarios = ", usuarios);
 
-      console.log(
-        `Apurando like de ${usuario.nome} ` +
-          `| ${usuarios.indexOf(usuario) + 1} de ${usuarios.length} `
-      );
-
-      if (!usuario.segue) {
-        usuarios_tmp[indice].mensagem.push("Não segue. ");
-      }
-
-      let deu_like = false;
-      const resposta_like = await get_likes({
-        params: {
-          id: usuario.id,
-        },
-      });
-
-      for (const video of resposta_like.data.data.list)
-        if (String(video.id) == String(video_id)) {
-          deu_like = true;
-          break;
-        }
-
-      if (deu_like) {
-        usuarios_tmp[indice].deu_like = true;
-      } else {
-        usuarios_tmp[indice].deu_like = false;
-        usuarios_tmp[indice].mensagem.push("Não deu like nos últimos 100 vídeos. ");
-      }
-      set_usuarios(usuarios_tmp);
-    }
-    
     return new Promise((resolve, reject) => {
-      resolve("ok");
+      (async () => {
+        try {
+          for (const usuario of usuarios) {
+            const usuarios_tmp = usuarios;
+            const indice = usuarios.indexOf(usuario);
+
+            console.log(
+              `Apurando like de ${usuario.nome} ` +
+                `| ${usuarios.indexOf(usuario) + 1} de ${usuarios.length} `
+            );
+
+            if (!usuario.segue) {
+              usuarios_tmp[indice].mensagem.push("Não segue. ");
+            }
+
+            let deu_like = false;
+            const resposta_like = await get_likes({
+              params: {
+                id: usuario.id,
+              },
+            });
+
+            for (const video of resposta_like.data.data.list)
+              if (String(video.id) == String(video_id)) {
+                deu_like = true;
+                break;
+              }
+
+            if (deu_like) {
+              usuarios_tmp[indice].deu_like = true;
+            } else {
+              usuarios_tmp[indice].deu_like = false;
+              usuarios_tmp[indice].mensagem.push(
+                "Não deu like nos últimos 100 vídeos. "
+              );
+            }
+            set_usuarios(usuarios_tmp);
+          }
+          resolve(usuarios); 
+        } catch (error) {
+          reject(error);
+        }       
+      })();
+      
+      
     });
   };
 
   const atribuir_tickets = () => {
-    console.log("atribuindo tickets...");
-    for (const usuario of usuarios) {
-      const condicoes =
-        usuario.segue &&
-        usuario.deu_like &&
-        usuario.comentou &&
-        usuario.nome_valido;
-
-      if (condicoes) usuario.tickets = 1;
-    }
-    
     return new Promise((resolve, reject) => {
-      resolve("ok");
+      try {
+        console.log("atribuindo tickets...");
+        for (const usuario of usuarios) {
+          const condicoes =
+            usuario.segue &&
+            usuario.deu_like &&
+            usuario.comentou &&
+            usuario.nome_valido;
+
+          if (condicoes) usuario.tickets = 1;
+        }
+        resolve(usuarios);
+        
+      } catch (error) {
+        reject(error);
+        
+      }
+      
     });
   };
 
-  const checar_vip = async () => {
-    console.log("checando gift votes...");
-    const resposta_gift_votes = await get_gift_votes();
-
-    for (const vip of resposta_gift_votes.data.data.reward_rank) {
-      let ja_existe = false;
-      let indice = 0;
-
-      for (const usuario of usuarios) {
-        if (vip.uid == String(usuario.id)) {
-          ja_existe = true;
-          indice = usuarios.indexOf(usuario);
-        }
-      }
-
-      if (!ja_existe) {
-        let vip_estruturado = {
-          nome: vip.nickname,
-          id: vip.uid,
-          tickets: 0,
-          segue: false,
-          deu_like: false,
-          comentou: false,
-          mensagem: [],
-        };
-        vip_estruturado.mensagem.push(
-          "Fez doação mas" +
-            " não completou todas as tarefas." +
-            " Nenhum ticket adicionado. "
-        );
-        set_usuarios(old_usuarios => [...old_usuarios, vip_estruturado]);
-      } else {
-        const condicoes =
-          usuarios[indice].segue &&
-          usuarios[indice].deu_like &&
-          usuarios[indice].comentou &&
-          usuarios[indice].nome_valido;
-
-        if (condicoes) {
-          let usuarios_tmp = [...usuarios];
-          usuarios_tmp[indice].tickets += parseInt(vip.score);
-          set_usuarios(usuarios_tmp);
-        } else {
-          let usuarios_tmp = [...usuarios];
-          usuarios_tmp[indice].mensagem.push(
-            "Fez doação mas" +
-              " não completou todas as tarefas." +
-              " Nenhum ticket adicionado. "
-          );
-          set_usuarios(usuarios_tmp);          
-        }
-      }
-    }
-    
+  const checar_vip = () => {
     return new Promise((resolve, reject) => {
-      resolve("ok");
+      (async () => {
+        try {
+          console.log("checando gift votes...");
+          const resposta_gift_votes = await get_gift_votes();
+
+          for (const vip of resposta_gift_votes.data.data.reward_rank) {
+            let ja_existe = false;
+            let indice = 0;
+
+            for (const usuario of usuarios) {
+              if (vip.uid == String(usuario.id)) {
+                ja_existe = true;
+                indice = usuarios.indexOf(usuario);
+              }
+            }
+
+            if (!ja_existe) {
+              let vip_estruturado = {
+                nome: vip.nickname,
+                id: vip.uid,
+                tickets: 0,
+                segue: false,
+                deu_like: false,
+                comentou: false,
+                nome_valido: false,
+                mensagem: [],
+              };
+              vip_estruturado.mensagem.push(
+                "Fez doação mas" +
+                  " não completou todas as tarefas." +
+                  " Nenhum ticket adicionado. "
+              );
+              set_usuarios((old_usuarios) => [
+                vip_estruturado,
+                ...old_usuarios,
+              ]);
+            } else {
+              const condicoes =
+                usuarios[indice].segue &&
+                usuarios[indice].deu_like &&
+                usuarios[indice].comentou &&
+                usuarios[indice].nome_valido;
+
+              if (condicoes) {
+                let usuarios_tmp = [...usuarios];
+                usuarios_tmp[indice].tickets += parseInt(vip.score);
+                set_usuarios(usuarios_tmp);
+              } else {
+                let usuarios_tmp = [...usuarios];
+                usuarios_tmp[indice].mensagem.push(
+                  "Fez doação mas" +
+                    " não completou todas as tarefas." +
+                    " Nenhum ticket adicionado. "
+                );
+                set_usuarios(usuarios_tmp);
+              }
+            }
+          }
+          resolve(usuarios);
+          
+        } catch (error) {
+          reject(error);
+        }
+      })();
+      
     });
   };
-
-  
 
   const executar_tudo = async () => {
     console.log("iniciando API");
@@ -256,10 +314,11 @@ export default function Home() {
     await checar_likes();
     await atribuir_tickets();
     await checar_vip();
-    console.log("usuarios usuarios", usuarios);
-  }
+  };
 
-  
+  const mostrar_usuarios = () => {
+    console.log("usuarios usuarios", usuarios);
+  };
 
   return (
     <div>
@@ -279,7 +338,15 @@ export default function Home() {
       <main>
         <h1>Sorteador</h1>
         <button onClick={executar_tudo}>executar tudo</button>
-        <>{usuarios.map((usuario) => <p key={usuario.id}> {`${usuario.nome} ${usuario.mensagem}`} </p>, <br/> )}</>
+        <>
+          {usuarios.map(
+            (usuario) => (
+              <p key={usuario.id}>{`${usuario.nome} ${usuario.mensagem}`}</p>
+            ),
+            <br />
+          )}
+        </>
+        <button onClick={mostrar_usuarios}>mostrar usuarios</button>
       </main>
 
       <footer></footer>
